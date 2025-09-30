ddad58e37aa302e61b0f19edac02debffb4ccf83
hminq <hminh250104@gmail.com>
2025-09-30 15:39:47 +0700

    Refactor: Move SignUpFragment and implement SignUp screen
    - Moved SignUpFragment to ui.signup package.
    - Implemented SignUp screen UI and logic.
    - Created SignUpViewModel to handle sign-up process and validation.
    - Added navigation to Sign In and Welcome screens.
    - Displayed error messages for validation and sign-up failures.
    - Navigated to Home screen on successful sign-up.

---
+++ app/src/main/java/com/hminq/quizlett/ui/signup/SignUpViewModel.java
@@ -0,0 +1,62 @@
+package com.hminq.quizlett.ui.signup;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.hminq.quizlett.data.repository.UserRepository;
+import com.hminq.quizlett.exceptions.ValidationException;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+
+@HiltViewModel
+public class SignUpViewModel extends ViewModel {
+    private static final String TAG = "SIGNUP_VIEWMODEL";
+    private final MutableLiveData<Throwable> signUpError = new MutableLiveData<>();
+    private final MutableLiveData<ValidationException> validationException = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> isUserSignedUp = new MutableLiveData<>(false);
+    private final UserRepository userRepository;
+    private final CompositeDisposable disposables = new CompositeDisposable();
+
+    @Inject
+    public SignUpViewModel(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void signUp(String email, String password, String fullname) {
+        disposables.add(
+                userRepository.signUp(email, password, fullname)
+                        .subscribe(authResult -> {
+                            isUserSignedUp.postValue(true);
+                        }, throwable -> {
+                            if (throwable instanceof ValidationException) {
+                                validationException.postValue((ValidationException) throwable);
+                            } else {
+                                // Firebase error
+                                signUpError.postValue(throwable);
+                            }
+                        })
+        );
+    }
+
+    public LiveData<Throwable> getSignUpErrorLiveData() {
+        return this.signUpError;
+    }
+
+    public LiveData<ValidationException> getValidationExceptionLiveData() {
+        return this.validationException;
+    }
+
+    public LiveData<Boolean> getIsUserSignedUpLiveData() {
+        return this.isUserSignedUp;
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        disposables.clear();
+    }
+}
