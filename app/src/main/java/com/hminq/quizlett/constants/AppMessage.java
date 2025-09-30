cd28168552ed219e5ea1f42c841f1128c41fc669
hminq <hminh250104@gmail.com>
2025-09-30 15:37:24 +0700

    Add AppMessage constants

---
+++ app/src/main/java/com/hminq/quizlett/constants/AppMessage.java
@@ -0,0 +1,11 @@
+package com.hminq.quizlett.constants;
+
+import com.hminq.quizlett.R;
+
+public class AppMessage {
+    public static final int EMAIL_SENT_SUCCESS = R.string.email_sent_success;
+    public static final int EMAIL_SENT_ERROR = R.string.email_sent_error;
+    public static final int SIGNIN_ERROR = R.string.signIn_error;
+    public static final int SIGNUP_ERROR = R.string.signUp_error;
+    public static final int SIGNUP_SUCCESS = R.string.signUp_error;
+}
