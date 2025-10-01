cb7694f31425778a58cc661a1ecb8f816e4ad889
hminq <hminh250104@gmail.com>
2025-10-01 21:25:11 +0700

    Refactor navigation and add ContainerFragment
    This commit introduces `ContainerFragment` to manage tabbed navigation and refactors the existing navigation graph.

---
+++ app/src/main/java/com/hminq/quizlett/ui/ContainerFragment.java
@@ -0,0 +1,85 @@
+package com.hminq.quizlett.ui;
+import static com.hminq.quizlett.constants.ResourceConstant.TAB1_ICON;
+import static com.hminq.quizlett.constants.ResourceConstant.TAB2_ICON;
+import static com.hminq.quizlett.constants.ResourceConstant.TAB3_ICON;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.hminq.quizlett.R;
+import com.hminq.quizlett.databinding.FragmentContainerBinding;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class ContainerFragment extends Fragment {
+    private FragmentContainerBinding binding;
+    private SharedViewModel sharedViewModel;
+    private NavController navController;
+    private ViewPager2 viewPager;
+    private TabLayout tabLayout;
+    private ViewPagerAdapter viewPagerAdapter;
+
+    public ContainerFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentContainerBinding.inflate(inflater, container, false);
+        navController = NavHostFragment.findNavController(this);
+        viewPager = binding.viewPager;
+        tabLayout = binding.tabLayout;
+
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            //set tab icon
+            switch (position) {
+                case 0:
+                    tab.setIcon(TAB1_ICON); // tab1 always selected by default
+                    break;
+                case 1:
+                    tab.setIcon(TAB2_ICON);
+                    break;
+                case 2:
+                    tab.setIcon(TAB3_ICON);
+                    break;
+                default:
+                    break;
+            }
+        }).attach();
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (sharedViewModel.getCurrentUserLiveData().getValue() == null) {
+            sharedViewModel.getCurrentUser();
+        }
+    }
+}
---
+++ app/src/main/java/com/hminq/quizlett/ui/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.hminq.quizlett.ui;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.hminq.quizlett.ui.firsttab.FirstTabFragment;
+import com.hminq.quizlett.ui.secondtab.SecondTabFragment;
+import com.hminq.quizlett.ui.thirdtab.ThirdTabFragment;
+
+public class ViewPagerAdapter extends FragmentStateAdapter  {
+    private static final int TAB_NUMS = 3;
+
+    public ViewPagerAdapter(Fragment fragment) {
+        super(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new FirstTabFragment();
+            case 1:
+                return new SecondTabFragment();
+            case 2:
+                return new ThirdTabFragment();
+            default:
+                throw new IllegalArgumentException("Invalid tab position: " + position);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return TAB_NUMS;
+    }
+}
