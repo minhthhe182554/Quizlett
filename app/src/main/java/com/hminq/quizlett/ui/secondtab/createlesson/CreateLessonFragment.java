393e8be8c9e4e0e5c57b4e6e1f2fd5070067661f
hminq <hminh250104@gmail.com>
2025-10-01 21:26:14 +0700

    Add CreateLessonFragment and layout
    This commit introduces the `CreateLessonFragment` class and its corresponding layout file `fragment_create_lesson.xml`.
    
    Key changes:
    - Created `CreateLessonFragment.java` which extends `Fragment`.
    - Implemented `onCreateView` to inflate the `fragment_create_lesson.xml` layout.
    - Created `fragment_create_lesson.xml` with a basic FrameLayout containing a TextView.

---
+++ app/src/main/java/com/hminq/quizlett/ui/secondtab/createlesson/CreateLessonFragment.java
@@ -0,0 +1,28 @@
+package com.hminq.quizlett.ui.secondtab.createlesson;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hminq.quizlett.R;
+
+public class CreateLessonFragment extends Fragment {
+
+    public CreateLessonFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_lesson, container, false);
+    }
+}
